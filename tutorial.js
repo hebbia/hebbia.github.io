ba574ff090ddfdbe4e3bfc389303e84ecce7e43b
yohanstra <jreinstra@gmail.com>
2020-10-18 00:10:23 -0700

    save

--- tutorial.js
+++ tutorial.js
@@ -1,17 +1,18 @@
-// chrome.runtime.onMessageExternal.addListener(
-//   function(request, sender, sendResponse) {
-//     console.log("Received message!");
-//     console.log(request, sender, sendResponse);
-//   });
-
 var state = "new";  // new -> extension_opened ->
 
 window.addEventListener("HebbiaExtension", function(event) {
     if(event.detail.type === "popupOpen" && state === "new") {
-        console.log($("#hebbiaDiv1").position())
-        $("#hebbiaDiv1").removeClass("hebbiaDivSelected");
-        $("#hebbiaDiv1").animate({position: 'absolute', margin: 'inherit', left: 'inherit', right: '70px'}, "slow");
-        $("#hebbiaDiv2").show().addClass("hebbiaDivSelected");
-        state = "extension_opened";
+        setTimeout(function() {
+            $("#hebbiaDiv1 h1").html("Now go fuck yourself");
+            $("#hebbiaDiv1 span").html("you piece of shit");
+            state = "extension_opened";
+        }, 750);
+    }
+    if(event.detail.type === "popupOpen" && state === "new") {
+        setTimeout(function() {
+            $("#hebbiaDiv1 h1").html("Now go fuck yourself");
+            $("#hebbiaDiv1 span").html("you piece of shit");
+            state = "extension_opened";
+        }, 750);
     }
 });
