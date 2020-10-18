eb22cd59e39d567c3eb8f2642480c50411b84997
yohanstra <jreinstra@gmail.com>
2020-10-18 02:56:13 -0700

    save

--- tutorial.js
+++ tutorial.js
@@ -1,16 +1,18 @@
 var state = "new";  // new -> extension_opened ->
 
 window.addEventListener("HebbiaExtension", function(event) {
-    if(event.detail.type === "popupOpen" && state === "new") {
-        $("#hebbiaDiv1 h1").html("Hebbia is in-page search that <em>understands</em>.<br><br>Try it out:");
+    if (event.detail.type === "popupOpen" && state === "new") {
+        $("#hebbiaDiv1 h1").html("Hebbia is in-page search that <em>understands</em>.<br><br>Try it out by typing:");
         $("#hebbiaDiv1 span").html(
             "<div class=\"button\">What are the symptoms?</div>"
             + "<div class=\"button\">How contagious is Covid-19?</div>"
-            + "<div class=\"button\">Did COVID-19 come from a Chinese lab?</div>"
+            + "<div class=\"button\">Did it come from a lab?</div>"
         );
-        $(".button").click(function(event) {
-            console.log("click event!", event)
-        });
+        $("#hebbiaDiv1").removeClass("hebbiaDivSelected");
+        setTimeout(function() {
+            var event = new CustomEvent("HebbiaExtension", {detail: {type: "tutorialTypeQuestion", text: "Did it come from a lab?"}});
+            window.dispatchEvent(event);
+        }, 2000);
         state = "extension_opened";
     }
 });
