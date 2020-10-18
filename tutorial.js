9e34a13c59ff3886f48f9800c1a4eee139599d2a
yohanstra <jreinstra@gmail.com>
2020-10-18 01:23:22 -0700

    save with shitty buttons

--- tutorial.js
+++ tutorial.js
@@ -2,24 +2,12 @@ var state = "new";  // new -> extension_opened ->
 
 window.addEventListener("HebbiaExtension", function(event) {
     if(event.detail.type === "popupOpen" && state === "new") {
-        setTimeout(function() {
-            $("#hebbiaDiv1 h1").html("Hebbia is in-page search that <em>understands</em>.<br><br>Try it out:");
-            $("#hebbiaDiv1 span").html(
-                "Type a question into the search bar:<br><br>"
-                // + "<ul>"
-                + "<button>What are the symptoms?</button>"
-                + "<br>- How contagious is Covid-19?"
-                + "<br>- Did COVID-19 come from a Chinese lab?"
-                // + "</ul>"
-            );
-            state = "extension_opened";
-        }, 0);
+        $("#hebbiaDiv1 h1").html("Hebbia is in-page search that <em>understands</em>.<br><br>Try it out:");
+        $("#hebbiaDiv1 span").html(
+            "<div class=\"button\">What are the symptoms?</div>"
+            + "<div class=\"button\">How contagious is Covid-19?</div>"
+            + "<div class=\"button\">Did COVID-19 come from a Chinese lab?</div>"
+        );
+        state = "extension_opened";
     }
-    // if(event.detail.type === "popupOpen" && state === "new") {
-    //     setTimeout(function() {
-    //         $("#hebbiaDiv1 h1").html("Now go fuck yourself");
-    //         $("#hebbiaDiv1 span").html("you piece of shit");
-    //         state = "extension_opened";
-    //     }, 750);
-    // }
 });
