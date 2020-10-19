6b9a1a076fd8d0ca3cd1e0e9fdfff60a21984153
yohanstra <jreinstra@gmail.com>
2020-10-18 19:30:34 -0700

    Add OS toggle for keyboard shortcut

--- js/tutorial.js
+++ js/tutorial.js
@@ -30,3 +30,36 @@ window.addEventListener("HebbiaExtension", function(event) {
         }, 1000);
     }
 });
+
+
+// Taken from https://stackoverflow.com/questions/38241480/detect-macos-ios-windows-android-and-linux-os-with-js
+function getOS() {
+  var userAgent = window.navigator.userAgent,
+      platform = window.navigator.platform,
+      macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K', 'darwin'],
+      windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+      iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+      os = null;
+
+  if (macosPlatforms.indexOf(platform) !== -1) {
+    os = 'Mac';
+  } else if (iosPlatforms.indexOf(platform) !== -1) {
+    os = 'iOS';
+  } else if (windowsPlatforms.indexOf(platform) !== -1) {
+    os = 'Windows';
+  } else if (/Android/.test(userAgent)) {
+    os = 'Android';
+  } else if (!os && /Linux/.test(platform)) {
+    os = 'Linux';
+  }
+
+  return os;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    if (getOS() === "Mac") {
+        $("#hebbiaDiv1 span").html("Press CMD + H to begin searching.");
+    } else {
+        $("#hebbiaDiv1 span").html("Press Alt + H to begin searching.");
+    }
+});
