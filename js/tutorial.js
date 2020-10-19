7e04f3c5f59553f7a93e79838e6c7696ff23aa2c
yohanstra <jreinstra@gmail.com>
2020-10-19 04:08:26 -0700

    Add copy functionality

--- js/tutorial.js
+++ js/tutorial.js
@@ -4,14 +4,15 @@ window.addEventListener("HebbiaExtension", function(event) {
     if (event.detail.type === "popupOpen" && state === "new") {
         $("#hebbiaDiv1 h1").html("Hebbia is AI search that <em>understands</em>.<br><br>Try it out by typing:");
         $("#hebbiaDiv1 span").html(
-            "<div class=\"button\">What are the symptoms?</div>"
-            + "<div class=\"button\">How many people have been infected?</div>"
-            + "<div class=\"button\">Where did COVID originate?</div>"
+            "<div id=\"b1\" class=\"button\"><img src=\"img/icon_copy.svg\"> What are the symptoms?</div>"
+            + "<div id=\"b2\" class=\"button\"><img src=\"img/icon_copy.svg\"> How many people have been infected?</div>"
+            + "<div id=\"b3\" class=\"button\"><img src=\"img/icon_copy.svg\"> Where did COVID originate?</div>"
         );
         $("#wikiContent").animate({opacity: "100%"}, 3000);
         setTimeout(function() {
           typeQuestion("Where did COVID originate?", 0);
         }, 500);
+        addCopyListeners();
         // $("#wikiContent").removeClass("hidden");
         state = "extension_opened";
     }
@@ -35,40 +36,58 @@ window.addEventListener("HebbiaExtension", function(event) {
 });
 
 
+function addCopyListeners() {
+    $(".button").click(function(event) {
+        console.log(event);
+        $("#copyHelper").val(event.currentTarget.innerText).focus().select();
+        var $temp = $("<input>");
+        $("body").append($temp);
+        $temp.val(event.currentTarget.innerText).select();
+        document.execCommand("copy");
+        $temp.remove();
+        var selector = "#" + event.currentTarget.id + " img";
+        $(selector).attr("src", "img/icon_done.svg");
+        setTimeout(function() {
+            $(selector).attr("src", "img/icon_copy.svg");
+        }, 1000);
+    });
+}
+
+
 function typeQuestion(question, index) {
-  if (index > question.length) {
-    return;
-  }
-  var event = new CustomEvent("HebbiaExtension", {detail: {type: "tutorialTypeQuestion", text: question.substr(0, index)}});
-  window.dispatchEvent(event);
-  setTimeout(function() {
-    typeQuestion(question, index + 1);
-  }, 100);
+    if (index > question.length) {
+        return;
+    }
+    var event = new CustomEvent("HebbiaExtension", {detail: {type: "tutorialTypeQuestion", text: question.substr(0, index)}});
+    window.dispatchEvent(event);
+    setTimeout(function() {
+        typeQuestion(question, index + 1);
+    }, 100);
 }
 
 
 // Taken from https://stackoverflow.com/questions/38241480/detect-macos-ios-windows-android-and-linux-os-with-js
 function getOS() {
-  var userAgent = window.navigator.userAgent,
-      platform = window.navigator.platform,
-      macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K', 'darwin'],
-      windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
-      iosPlatforms = ['iPhone', 'iPad', 'iPod'],
-      os = null;
+    var userAgent = window.navigator.userAgent,
+    platform = window.navigator.platform,
+    macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K', 'darwin'],
+    windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+    iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+    os = null;
 
-  if (macosPlatforms.indexOf(platform) !== -1) {
-    os = 'Mac';
-  } else if (iosPlatforms.indexOf(platform) !== -1) {
-    os = 'iOS';
-  } else if (windowsPlatforms.indexOf(platform) !== -1) {
-    os = 'Windows';
-  } else if (/Android/.test(userAgent)) {
-    os = 'Android';
-  } else if (!os && /Linux/.test(platform)) {
-    os = 'Linux';
-  }
+    if (macosPlatforms.indexOf(platform) !== -1) {
+        os = 'Mac';
+    } else if (iosPlatforms.indexOf(platform) !== -1) {
+        os = 'iOS';
+    } else if (windowsPlatforms.indexOf(platform) !== -1) {
+        os = 'Windows';
+    } else if (/Android/.test(userAgent)) {
+        os = 'Android';
+    } else if (!os && /Linux/.test(platform)) {
+        os = 'Linux';
+    }
 
-  return os;
+    return os;
 }
 
 document.addEventListener('DOMContentLoaded', function () {
