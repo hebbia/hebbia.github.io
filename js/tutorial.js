283be99fcba5b7b929c08c54305bdfa933e8cd1a
yohanstra <jreinstra@gmail.com>
2020-10-19 21:55:17 -0700

    Some improvements to tutorial page

--- js/tutorial.js
+++ js/tutorial.js
@@ -1,54 +1,55 @@
-var state = "new";  // new -> extension_opened -> question_typed -> done
+var state = "new";  // new -> extension_opened -> question_typed -> viewing_results -> done
 
 window.addEventListener("HebbiaExtension", function(event) {
+    console.log("received!", event);
     if (event.detail.type === "popupOpen" && state === "new") {
-        $("#hebbiaDiv1 h1").html("Let's try asking about symptoms on this long COVID article.");
-        $("#hebbiaDiv1 span").html(
-            ""
-        );
-
-        $("#wikiContent").animate({opacity: "100%"}, 3000);
-        setTimeout(function() {
-          typeQuestion("What are the symptoms?", 0);
-        }, 4000);
-
-        setTimeout(function() {
-            $("#hebbiaDiv1 span").html(
-                "Hit <span class=\"shortcut\">return</span> to search."
-            );
-        }, 7500);
-
-          
-        // $("#wikiContent").removeClass("hidden");
         state = "extension_opened";
-    }
+        setTimeout(function() {
+            $("#hebbiaDiv1 h1").html("Let's try asking about symptoms on this long COVID article.");
+            $("#hebbiaDiv1 span").html(
+                ""
+            );
 
-    if (event.detail.type === "queryTyped" && event.detail.text.length > 3 && state === "extension_opened") {
+            $("#wikiContent").animate({opacity: "100%"}, 3000);
+            setTimeout(function() {
+              typeQuestion("What are the symptoms?", 0);
+            }, 2500);
+
+            setTimeout(function() {
+                $("#hebbiaDiv1 span").html(
+                    "Hit <span class=\"shortcut\">return</span> to search."
+                );
+                state = "question_typed";
+            }, 5500);
+        }, 500);
+    }
+    if (event.detail.type === "enter" && state === "question_typed") {
+        state = "viewing_results";
         setTimeout(function() {
             $("#hebbiaDiv1 h1").html("Relevant sentences are highlighted. Enter your way through them.");
             $("#hebbiaDiv1 span").html(
                 "Click matches to rank them and update results."
                 + "<br /> Double click them to immediately see similar sentences. </div>"
             );
-            state = "question_typed";
-        }, 4000);
+        }, 500);
     }
-    if (event.detail.type === "enter" && state === "question_typed") {
+    if ((event.detail.type === "enter" || event.detail.type === "labelClicked") && state === "viewing_results") {
+        state = "done";
         setTimeout(function() {
             $("#hebbiaDiv1 h1").html("Ask Hebbia anything.");
             $("#hebbiaDiv1 span").html(
                 "<br /> Here are some examples:"
-                + "<div id=\"b2\" class=\"button\"><img src=\"img/icon_copy.svg\"> How many people have been infected?</div>"
+                + "<div id=\"b1\" class=\"button\"><img src=\"img/icon_copy.svg\"> How many people have been infected?</div>"
                 + "<div id=\"b2\" class=\"button\"><img src=\"img/icon_copy.svg\"> How is it transmitted?</div>"
                 + "<div id=\"b3\" class=\"button\"><img src=\"img/icon_copy.svg\"> Where did COVID originate?</div>"
-                + "<div id=\"b1\" class=\"button\"><img src=\"img/icon_copy.svg\"> Is it fake news? </div>"
-                + "<div id=\"b1\" class=\"button\"><img src=\"img/icon_copy.svg\"> What is the R number?</div>"
-                + "<div id=\"b1\" class=\"button\"><img src=\"img/icon_copy.svg\"> What are the economic effects?</div>"
-                + "<div id=\"b1\" class=\"button\"><img src=\"img/icon_copy.svg\"> What has the WHO said?</div>"
+                + "<div id=\"b4\" class=\"button\"><img src=\"img/icon_copy.svg\"> Is it fake news? </div>"
+                + "<div id=\"b5\" class=\"button\"><img src=\"img/icon_copy.svg\"> What is the R number?</div>"
+                + "<div id=\"b6\" class=\"button\"><img src=\"img/icon_copy.svg\"> What are the economic effects?</div>"
+                + "<div id=\"b7\" class=\"button\"><img src=\"img/icon_copy.svg\"> What has the WHO said?</div>"
+                + "<span class=\"subsubtitle\">copy to clipboard</span>"
                 );
             addCopyListeners();
-            state = "done";
-        }, 5000);
+        }, 500);
     }
 });
 
@@ -59,7 +60,7 @@ function addCopyListeners() {
         $("#copyHelper").val(event.currentTarget.innerText).focus().select();
         var $temp = $("<input>");
         $("body").append($temp);
-        $temp.val(event.currentTarget.innerText).select();
+        $temp.val(event.currentTarget.innerText.substr(1)).select();
         document.execCommand("copy");
         $temp.remove();
         var selector = "#" + event.currentTarget.id + " img";
